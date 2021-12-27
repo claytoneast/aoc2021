8629bc0fa50da24905ce14d068ea8126150291c2
clayton <claytoneast@gmail.com>
2021-12-27 11:10:54 -0800

    day6 pt2

--- day6.js
+++ day6.js
@@ -6,41 +6,23 @@ function part_one(data) {
   let seedCounters = data.map(x => parseInt(x))
   let arrCounts = [0, 0, 0, 0, 0, 0, 0, 0, 0]
   seedCounters.forEach(counter => arrCounts[counter] += 1)
-  console.log(`----- arrCounts: ${JSON.stringify(arrCounts, null, 4)}`)
   let next8Count = seedCounters[0]
-  for (let i = 0; i < 2; i++) {
+  for (let i = 0; i < 256; i++) {
     arrCounts.forEach((count, index) => {
-      const fishAge = index
-      console.log(`----- fishAge: ${JSON.stringify(fishAge, null, 4)}`)
-      if (fishAge === 0) {
+      if (index === 0) {
         next8Count = count
       }
-      console.log(`----- next8Count: ${JSON.stringify(next8Count, null, 4)}`)
-      const nextCountForPos = index === 8 ? next8Count : arrCounts[i + 1]
-      console.log(`----- nextCountForPos: ${JSON.stringify(nextCountForPos, null, 4)}`)
-      if (fishAge === 8) {
+      const nextCountForPos = index === 8 ? next8Count : arrCounts[index + 1]
+      if (index === 8) {
         arrCounts[index] = next8Count
+        arrCounts[6] += next8Count
       } else {
         arrCounts[index] = nextCountForPos
       }
     })
-
-    console.log(`----- arrCounts: ${JSON.stringify(arrCounts, null, 4)}`)
-
-    // const currLength = fishCounters.length
-    // for (let fishIndex = 0; fishIndex < currLength; fishIndex++) {
-    //   const age = fishCounters[fishIndex]
-    //   if (age === 0) {
-    //     fishCounters.push(8)
-    //     fishCounters[fishIndex] = 6
-    //   } else {
-    //     fishCounters[fishIndex] = fishCounters[fishIndex] - 1
-    //   }
-    // }
-    // console.log(`----- i: ${JSON.stringify(i, null, 4)}`)
-    // console.log(`----- currLength: ${JSON.stringify(currLength, null, 4)}`)
   }
-  // console.log(fishCounters.length)
+  const finalCount = arrCounts.reduce((prev, curr) => prev + curr)
+  console.log(`----- finalCount: ${JSON.stringify(finalCount, null, 4)}`)
 }
 
 part_one(data)
