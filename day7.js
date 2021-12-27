7b55446ae7cca97d6acd33dfd78e792ff0555712
clayton <claytoneast@gmail.com>
2021-12-27 11:34:59 -0800

    day7pt2 first try im a god until day8

--- day7.js
+++ day7.js
@@ -18,7 +18,38 @@ function part_one(data) {
       totalFuel[i] += fuelUsed
     })
   }
-  console.log(`----- totalFuel: ${JSON.stringify(totalFuel, null, 4)}`)
+
+  let lowestVal = [0, totalFuel[0]]
+  Object.entries(totalFuel).forEach(([key, value]) => {
+    if (value < lowestVal[1]) {
+      lowestVal[0] = key
+      lowestVal[1] = value
+    }
+  })
+}
+
+// part_one(data)
+
+function part_two(data) {
+  let min = 0
+  let max = 0
+  data.forEach(val => {
+    if (val < min) min = val
+    if (val > max) max = val
+  })
+
+  let totalFuel = {}
+  for (let i = min; i <= max; i++) {
+    totalFuel[i] = 0
+    data.forEach(val => {
+      const distanceTraveled = Math.abs(i - val)
+      let fuelBurned = 0
+      for (let j = 1; j <= distanceTraveled; j++) {
+        fuelBurned += j
+      }
+      totalFuel[i] += fuelBurned
+    })
+  }
 
   let lowestVal = [0, totalFuel[0]]
   Object.entries(totalFuel).forEach(([key, value]) => {
@@ -30,4 +61,4 @@ function part_one(data) {
   console.log(`----- lowestVal: ${JSON.stringify(lowestVal, null, 4)}`)
 }
 
-part_one(data)
+part_two(data)
