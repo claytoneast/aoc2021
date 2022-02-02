361d4915c2de42beceb26a9dcdf11a9bd15b6642
clayton <claytoneast@gmail.com>
2022-02-02 10:37:08 -0800

    go-ify the go

--- day1.go
+++ day1.go
@@ -1,42 +1,53 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"strconv"
 	"strings"
 )
 
-func main() {
+func day1() error {
 	bytes, err := ioutil.ReadFile("day1_input")
 	if err != nil {
-		fmt.Printf("---- err: %d", err)
+		return err
 	}
+
 	str := string(bytes)
 	str = strings.TrimSuffix(str, "\n")
 	splitted := strings.Split(str, "\n")
-	numbers := make([]int, len(splitted))
-	for i, val := range splitted {
-		var int, err = strconv.Atoi(val)
+	numbers := []int{}
+	for _, val := range splitted {
+		int, err := strconv.Atoi(val)
 		if err != nil {
-			fmt.Printf("---- err: %d", err)
+			return err
 		}
-		numbers[i] = int
+		numbers = append(numbers, int)
 	}
 
 	if len(splitted) != len(numbers) {
-		panic("yee")
+		return errors.New("splitted != numbers")
 	}
 
 	timesValsIncreased := 0
 	for i, val := range numbers {
-		if i > 0 {
-			prevVal := numbers[i - 1]
-			if val > prevVal {
-				timesValsIncreased++
-			}
+		if i == 0 { continue }
+
+		prevVal := numbers[i - 1]
+		if val > prevVal {
+			timesValsIncreased++
 		}
 	}
 
 	fmt.Printf("---- timesValsIncreased: %d\n", timesValsIncreased)
+	return nil
+}
+
+func main() {
+	err := day1()
+	if err != nil {
+		log.Fatalf("---- err: %v\n", err)
+	}
 }
